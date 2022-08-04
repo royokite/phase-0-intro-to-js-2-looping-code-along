ba371a23997267a0e645b9e7924067ac5783f3f8
RoyOkite <royokite@gmail.com>
2022-08-04 19:50:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+// JS looping code along!
+
+function writeCards(gifterName, eventName) {
+    const messages = [];
+
+    for (let i = 0; i < gifterName.length; i++) {
+        messages.push(`Thank you, ${gifterName[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+}
+
+function countDown(positiveInteger) {
+    let counter = 10;
+    while (counter >= 0) {
+        console.log(counter--)
+    }
+}
